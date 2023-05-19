d72f254d7564ea4eb41c4c78afa64bacf0c451c6
cyanic-selkie <cyanic-selkie@protonmail.com>
2023-05-19 13:06:31 +0200

    Adds evalution.

--- lm/lm/evaluate.py
+++ lm/lm/evaluate.py
@@ -3,32 +3,56 @@ from dataset import get_dataset_evaluate
 from itertools import groupby
 
 if __name__ == "__main__":
-    index, lemmatizer, tokenizer, model, nodes, embeddings = initialize_disambiguation()
+    index, lemmatizer, tokenizer, model, embeddings = initialize_disambiguation()
 
     dataset = get_dataset_evaluate()
 
-    for i, (key, group) in enumerate(groupby(iter(dataset["validation"]), key=lambda x: x["document_id"])):
-        for sentence in group:
-            text = sentence["text"]
-            spans = []
-            qids = []
-            for entity in sentence["entities"]:
+    micro_top5_tp = 0
+    micro_top1_tp = 0
+    micro_all = 0
+
+    macro_top5_all = []
+    macro_top1_all = []
+    for i, document in enumerate(iter(dataset["validation"])):
+        text = document["text"]
+        spans = []
+        qids = []
+        for entity in document["entities"]:
+            if entity["qid"]:
                 spans.append((entity["start"], entity["end"]))
                 qids.append(entity["qid"])
 
-            if len(spans) == 0:
-                continue
+        if len(spans) == 0:
+            continue
 
-            results = disambiguate(text, spans, 5, nodes, embeddings, index, tokenizer, model, lemmatizer)
-            result_qids = []
-            result_names = []
-            for result in results:
-                if len(result) > 0:
-                    result_qids.append([qid for qid, _, _, _ in result])
-                    result_names.append([name for _, name, _, _ in result])
-                else:
-                    result_qids.append([])
-                    result_names.append([])
+        predictions = disambiguate(text, spans, 5, embeddings, index, tokenizer, model, lemmatizer)
+        prediction_qids = []
+        prediction_names = []
+        for prediction in predictions:
+            prediction_qids.append([qid for qid, _, _, _ in prediction])
+            prediction_names.append([name for _, name, _, _ in prediction])
 
-            if i == 100:
-                breakpoint()
+        document_top5_tp = 0
+        document_top1_tp = 0
+        document_all = 0
+        for qid, prediction_qid, prediction_name, span in zip(qids, prediction_qids, prediction_names, spans):
+            if qid in prediction_qid:
+                micro_top5_tp += 1  
+                document_top5_tp += 1
+
+            if len(prediction_qid) > 0 and qid == prediction_qid[0]:
+                micro_top1_tp += 1
+                document_top1_tp += 1
+
+            micro_all += 1  
+            document_all += 1
+
+        macro_top5_all.append(document_top5_tp / document_all)
+        macro_top1_all.append(document_top1_tp / document_all)
+
+        breakpoint()
+
+    print(f"Macro Top 5 accuracy: {sum(macro_top5_all) / len(macro_top5_all)}")
+    print(f"Macro Top 1 accuracy: {sum(macro_top1_all) / len(macro_top1_all)}")
+    print(f"Micro Top 5 accuracy: {micro_top5_tp / micro_all}")
+    print(f"Micro Top 1 accuracy: {micro_top1_tp / micro_all}")
