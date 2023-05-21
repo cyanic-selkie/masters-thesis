101f6cca1abb7c3688b110af08bc775ff88a726e
cyanic-selkie <cyanic-selkie@protonmail.com>
2023-05-21 11:16:45 +0200

    Adds hyperparameter optimization.

--- lm/lm/finetune.py
+++ lm/lm/finetune.py
@@ -8,24 +8,27 @@ from torch import nn
 import numpy as np
 import os
 from math import ceil
+from ray.tune.schedulers import PopulationBasedTraining
+from ray import tune
+from transformers.optimization import AdamW, get_constant_schedule
 
-def train(model, checkpoint, tokenizer, name, batch_size, learning_rate, warmup_steps, gradient_accumulation_steps, embedding_size, dataset, continue_training, epochs):
+def train(model, checkpoint, tokenizer, name, embedding_size, dataset, epochs):
     data_collator = DataCollatorForEL(tokenizer, embedding_size)
 
     args = TrainingArguments(
         output_dir=f"models/{name}",
         evaluation_strategy="epoch",
-        learning_rate=learning_rate,
-        per_device_train_batch_size=batch_size,
-        per_device_eval_batch_size=batch_size,
+        # learning_rate=learning_rate,
+        # per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=32,
         num_train_epochs=epochs,
-        weight_decay=0.01,
+        # weight_decay=0.01,
         report_to="wandb",
         save_strategy="epoch",
         save_total_limit=epochs,
         logging_steps=1,
-        warmup_steps=warmup_steps,
-        gradient_accumulation_steps=gradient_accumulation_steps,
+        # warmup_steps=warmup_steps,
+        # gradient_accumulation_steps=gradient_accumulation_steps,
     )
 
     trainer = Trainer(
@@ -37,28 +40,31 @@ def train(model, checkpoint, tokenizer, name, batch_size, learning_rate, warmup_
         tokenizer=tokenizer,
     )
 
-    if continue_training:
-        trainer.train(checkpoint)
-    else:
-        trainer.train()
+    best_trial = trainer.hyperparameter_search(
+        hp_space=lambda trial: {
+            "learning_rate": tune.loguniform(1e-6, 1e-4),
+            "per_device_train_batch_size": tune.choice([16, 32])
+        },
+        direction="minimize",
+        backend="ray",
+        n_trials = 10,
+        scheduler=PopulationBasedTraining(metric="loss", mode="min"),
+    )
+
+    # trainer.train()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", type=str, required=True)
     parser.add_argument("--checkpoint", type=str, required=True)
     parser.add_argument("--embedding-size", type=int, required=True)
-    parser.add_argument("--learning-rate", type=float, required=True)
-    parser.add_argument("--warmup-steps", type=float, required=True)
-    parser.add_argument("--batch-size", type=int, required=True)
     parser.add_argument("--epochs", type=int, required=True)
-    parser.add_argument("--gradient-accumulation-steps", type=int, required=True)
     parser.add_argument("--embeddings", type=str, required=True)
     parser.add_argument("--nodes", type=str, required=True)
-    parser.add_argument("--continue-training", action="store_true")
     args = parser.parse_args()
 
     model, tokenizer = instantiate_model(args.checkpoint, args.embedding_size)
 
     dataset = get_dataset_conll(tokenizer, args.embedding_size, args.embeddings, args.nodes)
 
-    train(model, args.checkpoint, tokenizer, args.name, args.batch_size, args.learning_rate, args.warmup_steps, args.gradient_accumulation_steps, args.embedding_size, dataset, args.continue_training, args.epochs)
+    train(model, args.checkpoint, tokenizer, args.name, args.embedding_size, dataset, args.epochs)
