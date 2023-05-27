b81f2e04f8e0b545c7313c878c8271ddf667ad53
cyanic-selkie <cyanic-selkie@protonmail.com>
2023-05-27 16:34:58 +0200

    Formatting.

--- pbg/combiner/combiner/combine.py
+++ pbg/combiner/combiner/combine.py
@@ -1,8 +1,8 @@
 import argparse
-import os
 import h5py
 import numpy as np
 from os.path import join
+
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -13,13 +13,18 @@ def main():
 	parser.add_argument("--partitions", type=int, required=True)
 	args = parser.parse_args()
 
-	embeddings = np.memmap(args.output, dtype=np.float32, mode="w+", shape=(args.nodes, args.dimensions))
+	embeddings = np.memmap(args.output,
+	                       dtype=np.float32,
+	                       mode="w+",
+	                       shape=(args.nodes, args.dimensions))
 	idx = 0
 	for i in range(args.partitions):
-		with h5py.File(join(args.embeddings_dir, f"embeddings_all_{i}.v40.h5"), "r") as f:
+		with h5py.File(join(args.embeddings_dir, f"embeddings_all_{i}.v40.h5"),
+		               "r") as f:
 			partition = f["embeddings"][()]
-			embeddings[idx : idx + partition.shape[0]] = partition
-			idx += partition.shape[0]		
+			embeddings[idx:idx + partition.shape[0]] = partition
+			idx += partition.shape[0]
+
 
 if __name__ == "__main__":
 	main()
